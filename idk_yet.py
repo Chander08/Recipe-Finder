16cab4ce8915e0048d4b29b16574df5909823178
Chander08 <suhailchander101@gmail.com>
2022-05-05 20:32:32 -0400

    Create idk_yet.py

---
+++ idk_yet.py
@@ -0,0 +1,13 @@
+#steps I guess?
+
+'''
+1st we're gonna need to get recipes from some recipe site as well as have the information about all the ingredients
+need to find a website like that which has the api (the connecting part to access data from)
+
+Then we can have something which takes the ingredients as user input and looks in the data to see all recipes which match all
+requirements. 
+
+
+then, once we have that data we create a website? Might need html/javascript/css/ for that though. 
+
+'''
